6f5eb06b7ed6eaf3a52f1a2432725bc144460df4
Josh Shor <jpshor@gmail.com>
2019-04-20 18:36:36 -0400

    feat(ui): adds fontawesome for icons and toolbar styling

--- packages/aristotle/src/components/Toolbar.vue
+++ packages/aristotle/src/components/Toolbar.vue
@@ -1,23 +1,112 @@
 <template>
-  <div>
-    <button
-      :disabled="!editor.canUndo"
-      @click="relayCommand('UNDO')">
-      Undo
-    </button>
-    <button
-      :disabled="!editor.canRedo"
-      @click="relayCommand('REDO')">
-      Redo
-    </button>
-    <button
-      @click="relayCommand('CUT')">
-      Cut
-    </button>
-    <button
-      @click="relayCommand('PASTE')">
-      Paste
-    </button>
+  <div class="toolbar">
+    <div class="toolbar__group toolbar__group--left">
+      <!-- only temporary -->
+      <button
+        @click="$emit('openDocument')"
+        class="toolbar-button">
+        <i class="fas fa-folder-open" />
+      </button>
+      <div class="toolbar__separator" />
+      <button
+        :disabled="!editor.canUndo"
+        @click="relayCommand('UNDO')"
+        class="toolbar-button">
+        <i class="fas fa-undo" />
+      </button>
+      <button
+        :disabled="!editor.canRedo"
+        @click="relayCommand('REDO')"
+        class="toolbar-button">
+        <i class="fas fa-redo" />
+      </button>
+      <div class="toolbar__separator" />
+      <button
+        :disabled="!editor.canRedo"
+        @click="relayCommand('REDO')"
+        class="toolbar-button">
+        <i class="fas fa-cut" />
+      </button>
+      <button
+        :disabled="!editor.canRedo"
+        @click="relayCommand('REDO')"
+        class="toolbar-button">
+        <i class="fas fa-copy" />
+      </button>
+      <button
+        :disabled="!editor.canRedo"
+        @click="relayCommand('REDO')"
+        class="toolbar-button">
+        <i class="fas fa-paste" />
+      </button>
+      <button
+        :disabled="!editor.canRedo"
+        @click="relayCommand('REDO')"
+        class="toolbar-button">
+        <i class="fas fa-ban" />
+      </button>
+      <div class="toolbar__separator" />
+      <button
+        :disabled="!editor.canRedo"
+        @click="relayCommand('REDO')"
+        class="toolbar-button">
+        <i class="fas fa-arrows-alt" />
+      </button>
+      <button
+        :disabled="!editor.canRedo"
+        @click="relayCommand('REDO')"
+        class="toolbar-button">
+        <i class="fas fa-mouse-pointer" />
+      </button>
+      <div class="toolbar__separator" />
+      <button
+        :disabled="!editor.canRedo"
+        @click="relayCommand('REDO')"
+        class="toolbar-button">
+        <i class="fas fa-object-group" />
+      </button>
+      <button
+        :disabled="!editor.canRedo"
+        @click="relayCommand('REDO')"
+        class="toolbar-button">
+        <i class="fas fa-object-ungroup" />
+      </button>
+      <div class="toolbar__separator" />
+      <button
+        :disabled="true"
+        @click="relayCommand('CREATE_INTEGRATED_CIRCUIT')"
+        class="toolbar-button">
+        <i class="fas fa-table" />
+      </button>
+      <div class="toolbar__separator" />
+      <button
+        :disabled="true"
+        @click="relayCommand('CREATE_INTEGRATED_CIRCUIT')"
+        class="toolbar-button">
+        <i class="fas fa-microchip" />
+      </button>
+    </div>
+    <div class="toolbar__group toolbar__group--right">
+      <button
+        :disabled="true"
+        @click="relayCommand('TOGGLE_DEBUG')"
+        class="toolbar-button">
+        <i class="fas fa-bug" />
+      </button>
+      <div class="toolbar__separator" />
+      <button
+        :disabled="!editor.canRedo"
+        @click="relayCommand('RESET')"
+        class="toolbar-button toolbar-button--pull-right">
+        <i class="fas fa-fast-backward" />
+      </button>
+      <button
+        :disabled="!editor.canRedo"
+        @click="relayCommand('STEP')"
+        class="toolbar-button toolbar-button--pull-right">
+        <i class="fas fa-step-forward" />
+      </button>
+    </div>
   </div>
 </template>
 
@@ -44,3 +133,71 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+
+$color-bg-primary: #1D1E25;
+$color-bg-secondary: #333641;
+$color-bg-tertiary: #3D404B;
+$color-bg-quaternary: #454857;
+
+$color-primary: #fff;
+$color-secondary: #9ca0b1;
+
+$color-shadow: #000;
+
+$border-width: 1px;
+$scrollbar-width: 3px;
+
+.toolbar {
+  background-color: $color-bg-secondary;
+  border: $border-width solid $color-bg-secondary;
+  box-shadow: 0 0 $border-width $color-shadow;
+  padding: 0.25rem;
+  display: flex;
+
+  &__separator {
+    display: block;
+    background-color: $color-secondary;
+    height: 90%;
+    margin: 0.25rem;
+    width: 1px;
+  }
+
+  &__group {
+    flex: 1;
+    display: flex;
+    align-items: center;
+
+    &--right {
+      justify-content: flex-end;
+    }
+  }
+}
+
+.toolbar-button {
+  display: flex;
+  color: $color-primary;
+  padding: 0.25rem 0;
+  width: 1.85rem;
+  font-size: 1.25rem;
+  justify-content: center;
+  align-items: center;
+  box-sizing: border-box;
+  border: 0;
+  background: none;
+
+  &:disabled {
+    opacity: 0.25;
+  }
+
+  &:not(:disabled) {
+    &:hover {
+      background-color: $color-bg-quaternary;
+    }
+    &:active {
+      background-color: $color-bg-tertiary;
+    }
+  }
+}
+</style>
--- packages/aristotle/src/containers/ToolboxContainer.vue
+++ packages/aristotle/src/containers/ToolboxContainer.vue
@@ -1,7 +1,11 @@
 <template>
-  <div>
-    <img :src="buffer" class="draw2d_droppable ui-draggable" ref="svg" />
-    Nor
+  <div class="toolbox">
+    <div class="toolbox__border">
+      <div class="toolbox__pane">
+        <img :src="buffer" class="draw2d_droppable ui-draggable" ref="svg" />
+        Nor
+      </div>
+    </div>
   </div>
 </template>
 
@@ -17,3 +21,38 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+$color-bg-primary: #1D1E25;
+$color-bg-secondary: #333641;
+$color-bg-tertiary: #3D404B;
+$color-bg-quaternary: #454857;
+
+$color-primary: #fff;
+$color-secondary: #9ca0b1;
+
+$color-shadow: #000;
+
+$border-width: 1px;
+$scrollbar-width: 3px;
+
+.toolbox {
+  padding: 0.5rem 0 0.5rem 0.5rem;
+  height: 100%;
+  box-sizing: border-box;
+
+  &__border {
+    box-shadow: 0 0 $border-width $color-shadow;
+    box-sizing: border-box;
+    height: 100%;
+    border: $border-width solid $color-bg-tertiary;
+    background-color: $color-bg-quaternary;
+    padding: 3px;
+  }
+
+  &__pane {
+    height: 100%;
+    background-color: $color-primary;
+  }
+}
+</style>
