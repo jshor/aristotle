e7f611e3df235afb139ec699280d7c8be2a2a07f
Josh Shor <jpshor@gmail.com>
2019-05-27 20:18:41 -0400

    feat(docs): adds versioning display and related stylus markup

--- docs/.vuepress/components/Landing.vue
+++ docs/.vuepress/components/Landing.vue
@@ -7,11 +7,11 @@
             <img src="/assets/logo.svg" />
           </div>
           <div class="landing__title">
-            <h1>Build, simulate and debug digital logic circuits.</h1>
-            
-            <a href="/app/index.html" class="downloads__button">Try online</a>
+            <h1>Build, simulate, and debug digital logic circuits.</h1>
 
-            <span class="downloads__version">v0.9.0</span>
+            <h3>Aristotle is an open source digital logic simulator for Windows, Linux, and macOS.</h3>
+      
+            <downloads />
           </div>
         </div>
 
@@ -23,11 +23,15 @@
 </template>
 
 <script>
+import Downloads from './Downloads'
 import Hook from './Hook'
 
 export default {
   name: 'Landing',
-  components: { Hook },
+  components: {
+    Downloads,
+    Hook
+  },
   data () {
     return {
       loaded: false
@@ -41,95 +45,59 @@ export default {
 }
 </script>
 
-<style>
-.landing {
-  opacity: 0;
-  transition: opacity 0.5s;
-}
+<style lang="stylus">
+.landing
+  opacity 0
+  transition opacity 0.5s
+  position absolute
+  top 0
+  left 0
+  width 100%
+  height 100vh
+  display flex
+  box-sizing border-box
 
-.landing--loaded {
-  opacity: 1;
-}
+  &--loaded
+    opacity 1
 
-.downloads__button {
-  height: 2.5rem;
-  background-color: #3eaf7c;
-  color: #fff;
-  border-radius: 2rem;
-  line-height: 2.5rem;
-  padding: 0 2rem;
-  display: inline-block;
-  text-shadow: none;
-  text-transform: uppercase;
-  box-shadow: 2px 1px 1px #000;
-  transition: background-color 0.25s;
-}
+  &__hero
+    display flex
+    align-items center
+    justify-content center
+    flex 1
+    position relative
+    background-color $codeBgColor
+    box-shadow 0 0 50px 50px $codeBgColor inset
+    background-size 40px 40px
+    background-image linear-gradient(to right, #33363F 1px, transparent 1px), 
+        linear-gradient(to bottom, #33363F 1px, transparent 1px)
 
-.downloads__button:hover {
-  background-color: #4A906A;
-}
+  &__main
+    max-width $contentWidth
+    display flex
+    flex-direction row
 
-.downloads__version {
-  color: #3eaf7c;
-  padding-left: 2.5rem;
-}
+  &__logo
+    filter drop-shadow(2px 1px 2px $shadowColor)
 
-.landing {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100vh;
-  display: flex;
-  box-sizing: border-box;
-}
+  &__title
+    color #fff
+    padding 2rem
+    text-shadow 2px 1px 3px $shadowColor
 
-.landing__hero {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex: 1;
-  position: relative;
-  background-color: #1B1C24;
-  box-shadow: 0 0 50px 50px #1B1C24 inset;
-  background-size: 40px 40px;
-  background-image: linear-gradient(to right, #33363F 1px, transparent 1px), 
-      linear-gradient(to bottom, #33363F 1px, transparent 1px);
-}
+  &__title
+    h1, h3
+      font-weight normal
 
-.landing__main {
-  max-width: 800px;
-  display: flex;
-  flex-direction: row;
-}
+  @media (max-width 720px)
+    &__main
+      flex-direction column
 
-.landing__logo {
-  filter: drop-shadow( 2px 1px 2px #1B1B23);
-}
+    &__logo
+      margin auto
+      width 100%
+      max-width 250px
 
-.landing__title {
-  color: #fff;
-  padding: 2rem;
-  text-shadow: 2px 1px 3px #000;
-}
-
-.landing__title h1 {
-  font-weight: normal;
-}
-
-@media (max-width: 720px) {
-  .landing__main {
-    flex-direction: column;
-  }
-
-  .landing__logo {
-    margin: auto;
-    width: 100%;
-    max-width: 250px;
-  }
-
-  .landing__title {
-    text-align: center;
-  }
-}
+    &__title
+      text-align center
 </style>
