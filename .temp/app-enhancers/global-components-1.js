f8d08afe1779029c6d270a70be49014dcf5e7d97
Josh Shor <jpshor@gmail.com>
2019-12-27 11:43:47 -0300

    refactor: removes .temp directory

--- .temp/app-enhancers/global-components-1.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-Vue.component("Landing", () => import("C:\\Users\\Josh\\Documents\\aristotle\\docs\\.vuepress\\components\\Landing"))
-Vue.component("Downloads", () => import("C:\\Users\\Josh\\Documents\\aristotle\\docs\\.vuepress\\components\\Downloads"))
-Vue.component("Hook", () => import("C:\\Users\\Josh\\Documents\\aristotle\\docs\\.vuepress\\components\\Hook"))
-Vue.component("Badge", () => import("C:\\Users\\Josh\\Documents\\aristotle\\node_modules\\@vuepress\\theme-default\\global-components\\Badge"))
-
-
-export default {}
