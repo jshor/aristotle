075af9bf66c48d7e4161f20644def65734f7541d
Josh Shor <jpshor@gmail.com>
2022-04-25 22:08:16 -0400

    chore(deps): upgrades dependencies and removes unused ones

---
+++ packages/website/components/Hook.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="hook" :class="`hook--${type}`">
+    <svg width="16" height="70" class="hook__svg">
+      <line x1="8" x2="8" y1="2" y2="70" class="hook__wire" />
+      <circle cx="8" cy="8" r="8" class="hook__needle" />
+    </svg>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Hook',
+  props: {
+    type: {
+      type: String,
+      default: 'light'
+    }
+  }
+})
+</script>
+
+<style lang="scss">
+$accentColor: #3eaf7c;
+$textColor: #2c3e50;
+$borderColor: #eaecef;
+$codeBgColor: #1B1B23;
+$shadowColor: #000000;
+
+.hook {
+  position: absolute;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+
+  &--light {
+    bottom: 0;
+
+    .hook__wire {
+      stroke: $borderColor;
+    }
+  }
+
+  &--dark {
+    bottom: -70px;
+
+    .hook__wire {
+      stroke: $codeBgColor;
+    }
+
+    .hook__needle {
+      fill: $codeBgColor;
+      stroke: $borderColor;
+      cy: 62px;
+    }
+  }
+
+  &__wire {
+    stroke-width: 2px;
+    stroke-dasharray: 6;
+    animation: marching-ants 30s infinite linear;
+  }
+
+  &__needle {
+    fill: $borderColor;
+    stroke: $codeBgColor;
+    stroke-width: 2px;
+  }
+}
+
+@keyframes marching-ants {
+  to {
+    stroke-dashoffset: -1000;
+  }
+}
+</style>
---
+++ packages/website/components/Landing.vue
@@ -0,0 +1,124 @@
+<template>
+  <div>
+    <div class="landing" :class="{ 'landing--loaded': loaded }">
+      <section class="landing__hero">
+        <div class="landing__main">
+          <div class="landing__logo">
+            <img :src="$withBase('/assets/logo.svg')" />
+          </div>
+          <div class="landing__title">
+            <h1>Build, simulate, and debug digital logic circuits.</h1>
+
+            <h3>Aristotle is an open source digital logic simulator for Windows, Linux, and macOS.</h3>
+
+            <downloads />
+          </div>
+        </div>
+
+        <hook type="dark" />
+        <hook type="light" />
+      </section>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Downloads from './Downloads.vue'
+import Hook from './Hook.vue'
+
+export default defineComponent({
+  name: 'Landing',
+  components: {
+    Downloads,
+    Hook
+  },
+  data () {
+    return {
+      loaded: false
+    }
+  },
+  mounted () {
+    setTimeout(() => {
+      this.loaded = true
+    })
+  }
+})
+</script>
+
+<style lang="scss">
+$accentColor: #3eaf7c;
+$textColor: #2c3e50;
+$borderColor: #eaecef;
+$codeBgColor: #1B1B23;
+$shadowColor: #000000;
+
+.landing {
+  opacity: 0;
+  transition: opacity 0.5s;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  box-sizing: border-box;
+
+  &--loaded {
+    opacity: 1;
+  }
+
+  &__hero {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex: 1;
+    position: relative;
+    background-color: $codeBgColor;
+    box-shadow: 0 0 50px 50px $codeBgColor inset;
+    background-size: 40px 40px;
+    background-image:
+      linear-gradient(to right, #33363F 1px, transparent 1px),
+      linear-gradient(to bottom, #33363F 1px, transparent 1px);
+  }
+
+  &__main {
+    max-width: 100vw;
+    display: flex;
+    flex-direction: row;
+    max-width: 860px;
+  }
+
+  &__logo {
+    filter: drop-shadow(2px 1px 2px $shadowColor)
+  }
+
+  &__title {
+    color: #fff;
+    padding: 2rem;
+    text-shadow: 2px 1px 3px $shadowColor;
+  }
+
+  &__title {
+    h1, h3 {
+      font-weight: normal;
+    }
+  }
+
+  @media (max-width 720px) {
+    &__main {
+      flex-direction: column;
+    }
+
+    &__logo {
+      margin: auto;
+      width: 100%;
+      max-width: 250px;
+    }
+
+    &__title {
+      text-align: center;
+    }
+  }
+}
+</style>
