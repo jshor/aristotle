f9a2a676a846ec61ebcd991b4937652daddb3b44
Josh Shor <jpshor@gmail.com>
2023-09-17 17:18:02 -0400

    style(input): restyles input switches

--- src/components/item/elements/InputNode.vue
+++ src/components/item/elements/InputNode.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="input-node">
+  <!-- <div class="input-node">
     <button
       type="button"
       class="input-node__flipper"
@@ -16,7 +16,27 @@
       <span v-else-if="model === -1">OFF</span>
       <span v-else>?</span>
     </button>
+  </div> -->
+  <div class="switch"
+      :class="{
+        'switch--on': model === 1,
+        'switch--off': model === -1,
+        'switch--hi-z': model === 0
+      }">
+    <div
+      class="switch__inner"
+      :tabindex="0"
+      aria-role="switch"
+    >
+      <div class="switch__trigger"
+      @mousedown.stop="onInput"
+      @touchstart.stop="onInput">
+        <div class="switch__light"></div>
+        <div class="switch__characters"></div>
+      </div>
+    </div>
   </div>
+
 </template>
 
 <script lang="ts">
@@ -71,44 +91,116 @@ export default defineComponent({
 </script>
 
 <style lang="scss">
-.input-node {
-  width: 60px;
-  height: 60px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border: 1px solid var(--color-secondary);
-  background-color: var(--color-bg-secondary);
-  pointer-events: all;
-  box-sizing: border-box;
-  cursor: move;
+$color: #18ffa0; // Try other colors, like #18ffa0
+$rotation: 20deg;
+$pivot-distance: 20px;
+$width: 120px;
+$height: 1.5 * $width;
 
-  &__flipper {
+.switch {
+  pointer-events: all;
+  touch-action: all;
+  cursor: move;
+  padding: 15px;
+  box-sizing: border-box;
+  background-color: var(--color-bg-secondary);
+  border: 2px solid var(--color-secondary);
+  border-radius: 3px;
+
+  &--on .switch__trigger {
+    transform: translateZ($pivot-distance) rotateX($rotation);
+    box-shadow: 0 -10px 20px $color;
+    --the-color: var(--color-on);
+  }
+
+  &--off {
+    --the-color: var(--color-off);
+  }
+  &--hi-z {
+    --the-color: var(--color-hi-z);
+  }
+
+  &__inner {
     display: flex;
     align-items: center;
     justify-content: center;
-    width: 30px;
-    height: 30px;
-    cursor: pointer;
-    border: 1px solid var(--color-secondary);
-    color: #fff;
-    box-sizing: border-box;
-    font-family: 'Courier New', Courier, monospace;
-    font-size: 0.75em;
-    border-radius: 2px;
+    width: $width;
+    height: $height;
+    box-shadow:
+      0 0 5px 2px var(--color-secondary), // The surrounding shadow (second layer)
+      inset 0 0 2px 15px var(--color-bg-secondary), // The light gray frame
+      inset 0 0 2px 22px black; // The internal black shadow
+    border-radius: 5px;
+    background-color: var(--color-bg-secondary);
+    perspective: 700px;
+    zoom: 0.25;
 
-    &--on {
-      background-color: var(--color-on);
-      box-shadow: 0 0 20px var(--color-on);
-    }
-
-    &--off {
-      background-color: var(--color-off);
-    }
-
-    &--high-z {
-      background-color: var(--color-hi-z);
-    }
   }
+
+    &__trigger{
+      cursor: pointer;
+      transition: all 0.15s cubic-bezier(1, 0, 1, 1);
+      transform-origin: center center -#{$pivot-distance};
+      transform: translateZ($pivot-distance) rotateX(-$rotation);
+      transform-style: preserve-3d;
+      width: 100%;
+      height: 100%;
+      position: relative;
+      background: var(--the-color);
+      background-repeat: no-repeat;
+
+
+      &::before {
+        content: "";
+        transition: all 0.25s cubic-bezier(1, 0, 1, 1);
+        background: var(--the-color);
+        background-repeat: no-repeat;
+        width: 100%;
+        height: 50px;
+        transform-origin: top;
+        transform: rotateX(-90deg);
+        position: absolute;
+        top: 0;
+      }
+
+      &::after {
+        content: "";
+        transition: all 0.25s cubic-bezier(1, 0, 1, 1);
+        background: var(--the-color);
+        width: 100%;
+        height: 50px;
+        transform-origin: top;
+        transform: translateY(50px) rotateX(-90deg);
+        position: absolute;
+        bottom: 0;
+        box-shadow: 0 50px 8px 0px black, 0 80px 20px 0px rgba(black, 0.5);
+      }
+    }
+
+    &__light {
+      opacity: 0;
+      animation:  light-off 1s;
+      position: absolute;
+      width: 100%;
+      height: 100%;
+    }
+
+    &__characters {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+
+      // Drawing with gradients!
+      // Read more about it here: https://css-tricks.com/drawing-images-with-css-gradients/
+      // And check this collection out: https://a.singlediv.com/
+      background:
+      linear-gradient(white, white) 50% 20%/5% 20%, radial-gradient(circle, transparent 50%, white 10%, white 70%, transparent 72%) 50% 80%/35% 25%;
+      background-repeat: no-repeat;
+    }
+}
+
+@keyframes light-off {
+  0% {opacity: 1}
+  80% {opacity: 0}
 }
 </style>
